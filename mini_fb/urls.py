527f15cebb8e166fe2982053498a8a68fed56186
abdul-nur <anur2110@gmail.com>
2024-10-07 17:53:08 -0400

    fixed link issue

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('profiles/', views.profile_list, name='profile_list'),
-    path('', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('base.html', views.home),
+    path('', views.home, name='home'),
+    path('show_all_profiles/', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -6,12 +6,17 @@ from .models import Profile
 
 
 
-
+# Function-based view for the profile list
 def profile_list(request):
     profiles = Profile.objects.all()
     return render(request, 'mini_fb/profile_list.html', {'profiles': profiles})
 
+# Function-based view for the home page
+def home(request):
+    return render(request, 'mini_fb/base.html')  # Specify the template here
+
+# Class-based view to display all profiles
 class ShowAllProfilesView(ListView):
     model = Profile
-    template_name = 'mini_fb/show_all_profiles.html'
+    template_name = 'mini_fb/show_all_profiles.html'  # Ensure this file exists in mini_fb/templates/mini_fb/
     context_object_name = 'profiles'
