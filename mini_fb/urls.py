fefdf925fdc39a20f7ac4f6295a498504aa2d216
abdul-nur <anur2110@gmail.com>
2024-11-04 14:41:12 -0500

    home fix

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -9,8 +9,8 @@ from .models import Profile
 urlpatterns = [
     path('profiles/', views.profile_list, name='profile_list'),
     path('base.html', views.home),
-    #path('', views.home, name='home'),
-    path('', ProfileListView.as_view(), name='profile_list'),
+    path('', views.home, name='home'),
+    #path('', ProfileListView.as_view(), name='profile_list'),
     path('show_all_profiles/', views.ShowAllProfilesView.as_view(), name='show_all_profiles'),
     path('profile/<int:pk>/', views.ShowProfilePageView.as_view(), name='show_profile'),
     path('create_profile/', views.CreateProfileView.as_view(), name='create_profile'),
